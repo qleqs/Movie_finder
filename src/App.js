f67e85b10ddcfed87c5e0308fc0217e89113e785
Raqin Shikder <raqin2003@Gmail.com>
2022-05-18 21:40:39 -0700

    created react app with basic logic to search through movies using an api, and created basic title

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './app.css'
+import searchIcon from './search.svg'
+//103e17aa
+
+const API_URL = 'http://www.omdbapi.com?apikey=103e17aa';
+
+const App = () => {
+
+  const searchMovies = async (title) =>{
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  }
+  useEffect(() => {
+    searchMovies('spiderman');
+  },[]);
+    return (
+      <div className="app">
+        <h1>Movie Finder</h1>
+      </div>
+    );
 }
 
 export default App;
